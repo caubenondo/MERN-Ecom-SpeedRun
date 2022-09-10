831e0f4060c441bc3a2937387b11edf037e1112f
Hai Duong <caubenondo@gmail.com>
2022-09-10 01:19:13 -0700

    simple server and client

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,31 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+import Home from "./pages/Home";
+import ProductDetail from './pages/ProductDetail'
+
 function App() {
   return (
-    <>
+    <Router>
       <Header/>
-      <main>
+      <main className="py-4">
         <Container>
-        <h1>Hello World</h1>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/product/:id' element={<ProductDetail/>}></Route>
+          </Routes>
         </Container>
         
       </main>
       <Footer/>
-    </>
+    </Router>
   );
 }
 
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className="text-center py-3">
-                    Copyright &copy; Team HD 
+                    Copyright &copy; Team Hai | Pattrick | Marshall | Richmond | Cecelia 
                 </Col>
             </Row>
         </Container>
