571366b707e37e7ec1fe423f2be4be0227c2ff0e
Hai Duong <caubenondo@gmail.com>
2022-09-10 10:17:41 -0700

    deploy 3

--- server/server.js
+++ server/server.js
@@ -11,9 +11,23 @@ dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 connectDB()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
+// There is no 404 setup, so we send people back to Homepage
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 app.get("/", (req, res) => {
     res.send("API is running");
 });
+
+
 app.use('/api/products',productRoutes)
 
 app.use(notFound)
