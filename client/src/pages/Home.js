89aeb1dacbe7dbfc1ff351cae7fd00775cb0bab0
Hai Duong <caubenondo@gmail.com>
2022-09-12 17:54:06 -0700

    fix header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -34,7 +34,7 @@ function Home() {
 
     return (
         <>
-            <h1>Lastest Product</h1>
+            <h1>Services</h1>
             {loading ? (
                 <Spiner></Spiner>
             ) : error ? (
